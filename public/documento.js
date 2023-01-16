0cce597ee517a49e87d017d7d6459d1688ed4c4c
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2023-01-16 10:37:10 -0300

    acrescentando

---
+++ public/documento.js
@@ -0,0 +1,41 @@
+import { emitirExcluirDocumento, emitirTextoEditor, selecionarDocumento } from "./socket-front-document.js";
+
+const parametros = new URLSearchParams(window.location.search);
+const nomeDocumento = parametros.get('nome');
+
+const textoEditor = document.getElementById('editor-texto');
+const tituloDocumento = document.getElementById("titulo-documento");
+const botaoExcluir = document.getElementById("excluir-documento")
+
+
+tituloDocumento.textContent = nomeDocumento || "documento sem titulo";
+
+selecionarDocumento(nomeDocumento)
+
+textoEditor.addEventListener('keyup', ()=>{
+    emitirTextoEditor({
+        texto: textoEditor.value,
+         nomeDocumento,
+        });
+});
+
+function atualizaTextoEditor(texto){
+    textoEditor.value = texto;
+}
+
+botaoExcluir.addEventListener("click", ()=>{
+    emitirExcluirDocumento(nomeDocumento);
+})
+
+function alertarERedirecionar(nome){
+
+
+    if(nome === nomeDocumento){
+        alert(`Documento ${nome} excluido!`);
+        window.location.href = "/";
+    }
+    
+
+}
+
+export{atualizaTextoEditor, alertarERedirecionar};
---
+++ src/dbConnect.js
@@ -0,0 +1,20 @@
+import {MongoClient} from 'mongodb';
+
+const cliente = new MongoClient('mongodb+srv://alura:123@alura.9simjmj.mongodb.net/?retryWrites=true&w=majority')
+
+let documentosColecao;
+try {
+    await cliente.connect();
+
+    const db = cliente.db('alura-websockets');
+     documentosColecao = db.collection('documentos')
+    
+    console.log('Conectado ao banco de dados com sucessso!')
+    
+} catch (erro) {
+    console.log(erro);
+    
+}
+
+export {documentosColecao}
+
